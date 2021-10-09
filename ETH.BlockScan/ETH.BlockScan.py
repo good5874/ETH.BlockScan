0d5c5aa3c5721c35cb4dc0682456c18848f347f2
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2021-10-09 16:59:49 +0300

    delete PostgreSql

--- ETH.BlockScan/ETH.BlockScan.py
+++ ETH.BlockScan/ETH.BlockScan.py
@@ -19,15 +19,8 @@ import adodbapi
 conStr = "PROVIDER=SQLOLEDB;Data Source={0};Database={1}; \
        trusted_connection=yes;UID={2};PWD={3};".format("127.0.0.1","Exchange","exchange","exchange1")
 
-# PostgreSql
-serverPostgreSql = '127.0.0.1'
-databasePostgreSql = 'index'
-usernamePostgreSql = 'postgres'
-passwordPostgreSql = 'admin'
-portPostgreSql = '5432'
-
-
-startBlock =  13366439
+startBlock = 13366439
+currentBlock = 13366439
 pollingPeriod = 20
 
 
@@ -48,7 +41,7 @@ lfh.setFormatter(formatter)
 logger.addHandler(lfh)
 
 # Adds all transactions from Ethereum block
-def insertion(blockid, tr, addressRows):
+def insertion(blockid, tr, IncomeWallets):
     time = web3.eth.getBlock(blockid)['timestamp']
     for x in range(0, tr):
         trans = web3.eth.getTransactionByBlock(blockid, x)
@@ -73,62 +66,57 @@ def insertion(blockid, tr, addressRows):
         # Correct contract transfer transaction represents '0x' + 4 bytes
         # 'a9059cbb' + 32 bytes (64 chars) for contract address and 32 bytes
         # for its value
+      
         # Some buggy txs can break up Indexer, so we'll filter it
         if len(contract_to) > 128:
             logger.info('Skipping ' + str(txhash) + ' tx. Incorrect contract_to length: ' + str(len(contract_to)))
             contract_to = ''
             contract_value = ''
+        
         try:
-            if to in addressRows:
-                  cursorPSQL.execute(
-                    'INSERT INTO public.ethtxs(time, txfrom, txto, value, gas, gasprice, block, txhash, contract_to, contract_value, status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
-                    (time, fr, to, value, gas, gasprice, blockid, txhash, contract_to, contract_value, status))
-        except BaseException :
-            continue
+            index = IncomeWallets[2].index(to)
+            if index > -1:
+                try:
+                    conn_MSSQL = adodbapi.connect(conStr)
+                    cursor_MSSQL = conn_MSSQL.cursor()
+                except:
+                    logger.error("Unable to connect to database MsSql")
 
+                userId = IncomeWallets[1][index]
+                walletId = IncomeWallets[0][index]
+                z = cursor_MSSQL.callproc("CreateIncomeTransaction_UpdateBalance_CreateEvent", ("ETH", txhash, value, gas, fr, to, time, userId, walletId))
+                conn_MSSQL.commit()
+                cursor_MSSQL.close()
+                conn_MSSQL.close()
+                zx = 1
+        except BaseException as x: 
+            continue
 while True:
     try:
-        connPSQl = psycopg2.connect(
-        database = databasePostgreSql, 
-        user = usernamePostgreSql, 
-        password = passwordPostgreSql, 
-        host = serverPostgreSql, 
-        port = portPostgreSql
-        )
-        connPSQl.autocommit = True
-    except:
-        logger.error("Unable to connect to database PSQL")
+        startBlock = currentBlock
 
-    cursorPSQL = connPSQl.cursor()
-    cursorPSQL.execute('SELECT Max(block) from public.ethtxs')
-    maxblockindb = cursorPSQL.fetchone()[0]
-
-    # On first start, we index transactions from a block number you indicate
-    if maxblockindb is None:
-        maxblockindb = int(startBlock)
-
-    try:
         try:
             connMSSQL = adodbapi.connect(conStr)
             cursorMSSQL = connMSSQL.cursor()
         except:
             logger.error("Unable to connect to database MsSql")
 
-        cursorMSSQL.execute("SELECT Address from IncomeWallets Where CurrencyAcronim = 'ETH'")
-        addressRows = cursorMSSQL.fetchall()
+        cursorMSSQL.execute("SELECT * from IncomeWallets Where CurrencyAcronim = 'ETH'")
+        IncomeWallets = cursorMSSQL.fetchall()
         cursorMSSQL.close()
         connMSSQL.close()
 
-        for block in range(maxblockindb + 1, maxblockindb + 1000):
-            logger.info('Current best block in index: ' + str(maxblockindb))
+        for block in range(startBlock + 1, startBlock + 1000):
+            logger.info('Current best block in index: ' + str(block))
             transactions = web3.eth.getBlockTransactionCount(block)
             if transactions > 0:
-                insertion(block, transactions, addressRows.ado_results[0])
+                insertion(block, transactions, IncomeWallets.ado_results)
+            currentBlock = block
         else:
             logger.debug('Block ' + str(block) + ' does not contain transactions')
     except:
         logger.error("block does not exist")
 
-    cursorPSQL.close()
-    connPSQl.close()
     time.sleep(int(pollingPeriod))
+
+
